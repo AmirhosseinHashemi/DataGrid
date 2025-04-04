e78586fc2aab3da97fdc49161ac6ac2db81690a5
Amirhossein <4mirh03einhashemi@gmail.com>
2025-04-04 21:26:06 +0330

    feat(api): create update delete and get function api

--- src/api/services.ts
+++ src/api/services.ts
@@ -9,3 +9,73 @@ export async function getRowData() {
     throw new Error("Error getting products data");
   }
 }
+
+export async function getRowDataById(id: number) {
+  try {
+    const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+    if (!response.ok) {
+      throw new Error("Error getting products data");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    throw new Error(`Error getting row ${id} data`);
+  }
+}
+
+interface rowData {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
+
+export async function updateRowData(data: rowData) {
+  const option = {
+    method: "PUT",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  try {
+    const response = await fetch(
+      `https://fakestoreapi.com/products/${data.id}`,
+      option,
+    );
+    if (!response.ok) {
+      throw new Error("Error updating row data");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error updating row data");
+  }
+}
+
+export async function deleteRow(id: number) {
+  const option = {
+    method: "DELETE",
+  };
+
+  try {
+    const response = await fetch(
+      `https://fakestoreapi.com/products/${id}`,
+      option,
+    );
+    if (!response.ok) {
+      throw new Error("Error deleting product data");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error deleting row data");
+  }
+}
