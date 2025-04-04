f28e28d0d281c1c1678059088557ce8799196b06
Amirhossein <4mirh03einhashemi@gmail.com>
2025-04-04 21:26:48 +0330

    feat(root): add toast pakcage to the root

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <App />
       <ReactQueryDevtools />
+      <Toaster />
     </QueryClientProvider>
   </StrictMode>,
 );
